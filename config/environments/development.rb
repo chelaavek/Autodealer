e217131d566042b289c634d565f24314f8d6e286
chelaavek <47272816+chelaavek@users.noreply.github.com>
2020-05-04 21:45:02 +0300

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,8 +64,8 @@ Rails.application.configure do
       :address              => 'smtp.gmail.com',
       :port                 => 587,
       :domain               => 'gmail.com',
-      :user_name            => 'GlebK199973',
-      :password             => 'QWERTYqaz123$',
+      :user_name            => 'login',
+      :password             => 'password',
       :authentication       => 'login',
       :enable_starttls_auto => true
   }
